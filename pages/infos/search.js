50207f0a1ed6f2791c609b95bc22ca7ea2bc1240
bak3839 <bak3839@naver.com>
2023-04-10 02:52:41 +0900

    create title preview under the search bar

--- pages/infos/search.js
+++ pages/infos/search.js
@@ -1,7 +1,7 @@
 import styles from '@/styles/Home.module.css'
-import React from 'react';
 import movie from '../movie_data.json'
-import { useState, useEffect } from 'react';
+import { useState, useEffect, React } from 'react';
+import useDebounce from './useDebounce';
 
 function Card(props) {
     let url = "https://image.tmdb.org/t/p/w200";
@@ -10,23 +10,27 @@ function Card(props) {
     let page = '/infos/' + index;
 
     return (
-        <a href={page}>
-            <div className={styles.card}>
-                <img className={styles.image} src={url+path}></img>
-                <p>{props.title}</p>
-            </div>
-        </a>
+        <div className={styles.card_container}>
+            <a href={page}>
+                <div className={styles.card}>
+                    <img className={styles.image} src={url + path}></img>
+                    <p>{props.title}</p>
+                </div>
+            </a>
+        </div>
     )
 }
+
 
 function find(search_title) {
     let list = [];
     let title;
     let poster_path;
     let id;
-    
-    for(let i = 0; i < movie.movie_data.length; i++) {
-        if(movie.movie_data[i].title.includes(search_title)) {
+
+    for (let i = 0; i < movie.movie_data.length; i++) {
+        if (movie.movie_data[i].title.includes(search_title)) {
+            
             id = movie.movie_data[i].id;
             title = movie.movie_data[i].title;
             poster_path = movie.movie_data[i].poster_path;
@@ -37,28 +41,74 @@ function find(search_title) {
 
     return list;
 }
+/**
+ * 제목으로 연관 영화 검색해서 미리보기 리스트 반환
+ * @param {*} title 
+ */
+function searchList(title) {
+    let list = [];
+    let cnt = 0;
 
-function Main() {
-    const [flag,setFlag] = useState(true);
-    const [title, setTitle] = useState("");
-    const [list, setList] = useState(null);
+    for (let i = 0; i < movie.movie_data.length; i++) {
+        if(cnt > 10) break;
+
+        if (movie.movie_data[i].title.includes(title)) {
+            cnt = cnt + 1;
+
+            let page = '/infos/' + i;           
+            let title = movie.movie_data[i].title;
+            list.push(<a href={page}><li key={i}>{title}</li></a>);
+        }
+    }
 
     return (
-        <div>
-            <input type='text' value={title} className={styles.textBox} placeholder='영화 제목 입력' onChange={(e) => {
-                console.log(e.target.value);
-                setTitle(e.target.value);
-            }}></input>
+        <div className={styles.list}>
+            <ul key={1}>
+                {list}
+            </ul>
+        </div>
+    )
+}
 
-            <button className={styles.btn3d} onClick={() => {
-                console.log(title);
-                setList(find(title));
-                setFlag(false);              
-            }}>검색</button>
+function Main() {
+    const [flag, setFlag] = useState(true);
+    const [title, setTitle] = useState("");
+    const [list, setList] = useState(null);
+    const [movie, setMovie] = useState(null);
 
-            <div>
-                {flag ? <h1>영화 정보가 표시됩니다.</h1> : list}
+    const debounceValue = useDebounce(title);
+
+    useEffect(() => {
+        const getMovies = async () => {
+            console.log(debounceValue);
+            setList(searchList(debounceValue));
+            setFlag(false);
+        }
+        if (debounceValue) getMovies();
+    }, [debounceValue]);
+
+    return (
+        <div className={styles.test}>
+            <div className={styles.search}>
+                <div className={styles.searchBox}>
+                    <input type='text' value={title} className={styles.textBox} placeholder='영화 제목 입력' onChange={(e) => {
+                        setTitle(e.target.value);
+                    }}></input>
+                    <button className={styles.btn3d} onClick={() => {
+                        setMovie(find(title));
+                        setFlag(false);
+                    }}>검색</button>
+                    {flag ? null : list}
+                </div>
+            </div>              
+
+            <div className={styles.container}>
+                <div className={styles.grid}>
+                    {flag ? <h1>영화 정보가 표시됩니다.</h1> : movie}
+                    {/* {title ? list : <h1>영화 정보가 표시됩니다.</h1>} */}
+                </div>
             </div>
+
         </div>
     )
 }
